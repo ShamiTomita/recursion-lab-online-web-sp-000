905bf0ca59ad6a2caecf719672737a1f431eaba9
Shami Tomita <shamitomita@gmail.com>
2020-12-04 19:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function includesNumber(array, desiredIndex){
  }
  if (desiredIndex != num[0]){
    num.splice(0,1)
-  return includesNumber(num, desiredIndex);
+  return includesNumber(array, desiredIndex);
  }
  else return false
 }
