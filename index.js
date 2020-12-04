b733f2d0c221593bcf9af189bee84a7fa3862931
Shami Tomita <shamitomita@gmail.com>
2020-12-04 21:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,14 +64,12 @@ function maxOf(array){
 function includesNumber(array, desiredIndex){
  let arrayLength = array.length
  let num = array.slice()
- if (arrayLength > 1){
- if (desiredIndex == num[0]){
+ if (arrayLength == 1 || desiredIndex == num[0]){
    return true
  }
  if (desiredIndex != num[0] && array.length >= 1){
    num.splice(0,1);
  }
-}
  return includesNumber(num, desiredIndex)
 }
  
